b567698b24dcae42c2ad746303ccaeeb0ca5b8b3
AyushShende <ayushshende9@gmail.com>
2022-05-06 12:03:55 +0530

    first commit

---
+++ client/src/utils/Constants.js
@@ -0,0 +1,6 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x728Ae44c798dDaa1CD36B0458f0DF8783aBE3185";
+
+//0xffCdED254768A54af0BE9b4248d3e49D43AAc85B
